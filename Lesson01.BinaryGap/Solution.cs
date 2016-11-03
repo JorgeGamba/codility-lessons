a68f9b1a1cd7e656fb58337d3afbce7d68ef828d
JorgeGamba <jorgegamba@gmail.com>
2016-11-03 16:41:51 +0000

    Lesson 01 - BinaryGap

---
+++ Lesson01.BinaryGap/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace Lesson01.BinaryGap
+{
+    class Solution
+    {
+        public int solution(int N)
+        {
+            int zeroCount = 0;
+            int maxCount = 0;
+            bool counting = false;
+            while (N > 0)
+            {
+                int remain = N % 2;
+                if (remain == 1)
+                {
+                    if (zeroCount > maxCount)
+                        maxCount = zeroCount;
+                    zeroCount = 0;
+                    counting = true;
+                }
+                else if (counting)
+                {
+                    zeroCount++;
+                }
+                N /= 2;
+            }
+
+            return maxCount;
+        }
+    }
+}
---
+++ Lesson01.BinaryGap/SolutionSpecs.cs
@@ -0,0 +1,60 @@
+﻿using Machine.Specifications;
+
+namespace Lesson01.BinaryGap
+{
+    [Subject(typeof(Solution))]
+    public class SolutionSpecs
+    {
+        Because of = () =>
+            _result = _solution.solution(_number);
+
+        public class When_the_binary_has_no_zeros
+        {
+            Establish context = () => 
+                _number = 1;
+
+            It should_return_0 = () =>
+                _result.ShouldEqual(0);
+        }
+
+        public class When_the_binary_has_one_zero_but_its_not_close_by_ones
+        {
+            Establish context = () => 
+                _number = 2;
+
+            It should_return_0 = () =>
+                _result.ShouldEqual(0);
+        }
+
+        public class When_the_binary_has_one_binary_gap
+        {
+            Establish context = () => 
+                _number = 9;
+
+            It should_return_the_right_result = () =>
+                _result.ShouldEqual(2);
+        }
+
+        public class When_the_binary_has_two_binary_gaps
+        {
+            Establish context = () => 
+                _number = 1041;
+
+            It should_return_the_maximum_binary_gap = () =>
+                _result.ShouldEqual(5);
+        }
+
+        public class When_the_binary_its_from_the_maximum_integer
+        {
+            Establish context = () => 
+                _number = 2147483647;
+
+            It should_return_0 = () =>
+                _result.ShouldEqual(0);
+        }
+
+        static Solution _solution = new Solution();
+        static int _result;
+        static int _number;
+    }
+}
