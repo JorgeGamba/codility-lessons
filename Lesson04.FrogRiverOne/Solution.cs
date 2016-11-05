74af81f1966f298c2f8a7910d3757d024f04b192
JorgeGamba <jorgegamba@gmail.com>
2016-11-05 12:30:38 +0000

    Lesson 04 - FrogRiverOne

---
+++ Lesson04.FrogRiverOne/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace Lesson04.FrogRiverOne
+{
+    public class Solution
+    {
+        public int solution(int X, int[] A)
+        {
+            var arrayLength = A.Length;
+            if (arrayLength >= X)
+            {
+                int[] steps = new int[X];
+                int lastCompletedStep = 0;
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    int currentValue = A[i];
+                    if (currentValue <= X)
+                    {
+                        steps[currentValue - 1]++;
+                        lastCompletedStep = GetTheLastCompletedStepGiven(steps, lastCompletedStep);
+                        if (lastCompletedStep == X)
+                            return i;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        int GetTheLastCompletedStepGiven(int[] steps, int lastCompletedStep)
+        {
+            for (int i = lastCompletedStep; i <= steps.Length - 1; i++)
+            {
+                if (steps[i] == 0)
+                    return lastCompletedStep;
+                lastCompletedStep++;
+            }
+            return lastCompletedStep;
+        }
+    }
+}
+ 
---
+++ Lesson04.FrogRiverOne/SolutionSpecs.cs
@@ -0,0 +1,129 @@
+﻿using Machine.Specifications;
+
+namespace Lesson04.FrogRiverOne
+{
+    [Subject(typeof(Solution))]
+    public class SolutionSpecs
+    {
+        Because of = () => 
+            _result = _solution.solution(_stepsNeeded, _array);
+
+        public class When_X_equal_to_1_the_unique_element_in_the_array_is_1
+        {
+            Establish context = () =>
+            {
+                _stepsNeeded = 1;
+                _array = new[] {1};
+            };
+
+            It should_return_0 = () =>
+                _result.ShouldEqual(0);
+        }
+
+        public class When_X_equal_to_1_but_the_unique_element_in_the_array_is_grather_than_1
+        {
+            Establish context = () =>
+            {
+                _stepsNeeded = 1;
+                _array = new[] {2};
+            };
+
+            It should_return_minus_1 = () =>
+                _result.ShouldEqual(-1);
+        }
+
+        public class When_the_number_of_elements_of_the_array_is_minor_to_X
+        {
+            Establish context = () =>
+            {
+                _stepsNeeded = 2;
+                _array = new[] {1};
+            };
+
+            It should_return_minus_1 = () =>
+                _result.ShouldEqual(-1);
+        }
+
+        public class When_the_array_has_exactly_the_required_steps_and_ordered
+        {
+            Establish context = () =>
+            {
+                _stepsNeeded = 5;
+                _array = new[] {1, 2, 3, 4, 5};
+            };
+
+            It should_return_the_right_result = () =>
+                _result.ShouldEqual(4);
+        }
+
+        public class When_the_array_has_exactly_the_required_steps_but_unordered
+        {
+            Establish context = () =>
+            {
+                _stepsNeeded = 5;
+                _array = new[] {5, 2, 4, 1, 3};
+            };
+
+            It should_return_the_right_result = () =>
+                _result.ShouldEqual(4);
+        }
+
+        public class When_path_is_filled_after_some_redundancy_in_leaves
+        {
+            Establish context = () =>
+            {
+                _stepsNeeded = 5;
+                _array = new[] {1, 3, 1, 4, 2, 3, 5, 4};
+            };
+
+            It should_return_the_right_result = () =>
+                _result.ShouldEqual(6);
+        }
+
+        public class When_the_path_is_not_completed_with_several_provided_options
+        {
+            Establish context = () =>
+            {
+                _stepsNeeded = 5;
+                _array = new[] { 5, 2, 1, 3 };
+            };
+
+            It should_return_minus_1 = () =>
+                _result.ShouldEqual(-1);
+        }
+
+        public class When_it_presents_the_most_valid_extremme_case
+        {
+            Establish context = () =>
+            {
+                _stepsNeeded = 100000;
+                _array = new int[100000];
+                for (int i = 0; i < _array.Length - 1; i++)
+                    _array[i] = 100000 - i;
+                _array[_array.Length - 1] = 1;
+            };
+
+            It should_return_the_right_result = () =>
+                _result.ShouldEqual(100000 - 1);
+        }
+
+        public class When_it_presents_the_most_invalid_extremme_case
+        {
+            Establish context = () =>
+            {
+                _stepsNeeded = 100000;
+                _array = new int[100000];
+                for (int index = 0; index < _array.Length; index++)
+                    _array[index] = 100000;
+            };
+
+            It should_return_the_right_result = () =>
+                _result.ShouldEqual(-1);
+        }
+
+        static Solution _solution = new Solution();
+        static int _result;
+        static int _stepsNeeded;
+        static int[] _array;
+    }
+}
