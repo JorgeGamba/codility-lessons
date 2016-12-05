2084b50261f02ca26a73a42cff965715f1e31814
JorgeGamba <jorgegamba@gmail.com>
2016-12-05 10:49:23 +0000

    Lesson 05 - PassingCars

---
+++ Lesson05.PassingCars/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace Lesson05.PassingCars
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            int travelingToWestCount = 0;
+            int passingCarsCount = 0;
+            foreach (var item in A)
+            {
+                if (item == 0)
+                    travelingToWestCount++;
+                else
+                    passingCarsCount += travelingToWestCount;
+
+                if (passingCarsCount > 1000000000)
+                    return -1;
+            }
+
+            return passingCarsCount;
+        }
+    }
+}
+ 
---
+++ Lesson05.PassingCars/SolutionSpecs.cs
@@ -0,0 +1,111 @@
+﻿using Machine.Specifications;
+
+namespace Lesson05.PassingCars
+{
+    [Subject(typeof(Solution))]
+    public class SolutionSpecs
+    {
+        Because of = () =>
+            _result = _solution.solution(A);
+
+        public class on_provided_example
+        {
+            Establish context = () =>
+                A = new [] {0, 1, 0, 1, 1};
+
+            It should_return_the_expected_result = () =>
+                _result.ShouldEqual(5);
+        }
+
+        public class When_all_cars_travel_to_east
+        {
+            Establish context = () =>
+                A = new [] {0, 0, 0, 0, 0};
+
+            It should_return_0 = () =>
+                _result.ShouldEqual(0);
+        }
+
+        public class When_all_cars_travel_to_west
+        {
+            Establish context = () =>
+                A = new [] {1, 1, 1, 1, 1};
+
+            It should_return_0 = () =>
+                _result.ShouldEqual(0);
+        }
+
+        public class When_only_the_first_car_travel_to_east
+        {
+            Establish context = () =>
+                A = new [] {0, 1, 1, 1, 1};
+
+            It should_return_the_quantity_minus_1 = () =>
+                _result.ShouldEqual(4);
+        }
+
+        public class When_only_the_last_car_travel_to_west
+        {
+            Establish context = () =>
+                A = new [] {0, 0, 0, 0, 1};
+
+            It should_return_the_quantity_minus_1 = () =>
+                _result.ShouldEqual(4);
+        }
+
+        public class When_only_the_first_car_travel_to_west
+        {
+            Establish context = () =>
+                A = new [] {1, 0, 0, 0, 0};
+
+            It should_return_0 = () =>
+                _result.ShouldEqual(0);
+        }
+
+        public class When_only_the_last_car_travel_to_east
+        {
+            Establish context = () =>
+                A = new [] {1, 1, 1, 1, 0};
+
+            It should_return_0 = () =>
+                _result.ShouldEqual(0);
+        }
+
+        public class When_there_is_just_1_car_traveling_to_east
+        {
+            Establish context = () =>
+                A = new[] { 0 };
+
+            It should_return_0 = () =>
+                _result.ShouldEqual(0);
+        }
+
+        public class When_there_is_just_1_car_traveling_to_west
+        {
+            Establish context = () =>
+                A = new[] { 1 };
+
+            It should_return_0 = () =>
+                _result.ShouldEqual(0);
+        }
+
+        public class When_it_passes_the_1000000000_passing_cars
+        {
+            Establish context = () =>
+            {
+                A = new int[100000];
+                for (int i = 0; i < 50000; i++)
+                    A[i] = 0;
+                for (int i = 50000; i < 100000; i++)
+                    A[i] = 1;
+            };
+
+            It should_return_minus_1 = () =>
+                _result.ShouldEqual(-1);
+        }
+
+        static Solution _solution = new Solution();
+        static int[] A;
+        static int _result;
+    }
+}
