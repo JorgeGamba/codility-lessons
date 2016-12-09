aef8ccfefd9053fb0015cdca62be502600dc7558
JorgeGamba <jorgegamba@gmail.com>
2016-12-09 08:25:41 +0000

    Lesson 05 - MinAvgTwoSlice

---
+++ Lesson05.MinAvgTwoSlice/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace Lesson05.MinAvgTwoSlice
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            float currentMinAverage = (float) (A[0] + A[1]) / 2;
+            int currentMinAveragePosition = 0;
+            float currentLastTwoAverage, currentLastThreeAverage;
+            for (int i = 2; i < A.Length; i++)
+            {
+                currentLastTwoAverage = (float) (A[i] + A[i - 1]) / 2;
+                if (currentLastTwoAverage < currentMinAverage)
+                {
+                    currentMinAveragePosition = i - 1;
+                    currentMinAverage = currentLastTwoAverage;
+                }
+                currentLastThreeAverage = (float) (A[i] + A[i - 1] + A[i - 2]) / 3;
+                if (currentLastThreeAverage < currentMinAverage)
+                {
+                    currentMinAveragePosition = i - 2;
+                    currentMinAverage = currentLastThreeAverage;
+                }
+            }
+
+            return currentMinAveragePosition;
+        }
+    }
+}
+ 
---
+++ Lesson05.MinAvgTwoSlice/SolutionSpecs.cs
@@ -0,0 +1,82 @@
+﻿using Machine.Specifications;
+
+namespace Lesson05.MinAvgTwoSlice
+{
+    [Subject(typeof(Solution))]
+    public class SolutionSpecs
+    {
+        Because of = () =>
+            _result = _solution.solution(A);
+
+        public class on_provided_example
+        {
+            Establish context = () =>
+                A = new [] {4, 2, 2, 5, 1, 5, 8};
+
+            It should_return_the_expected_result = () =>
+                _result.ShouldEqual(1);
+        }
+
+        public class When_all_elements_have_the_same_value
+        {
+            Establish context = () =>
+                A = new [] {4, 4, 4, 4, 4};
+
+            It should_return_0 = () =>
+                _result.ShouldEqual(0);
+        }
+
+        public class When_the_array_is_a_sequence
+        {
+            Establish context = () =>
+                A = new [] {-2, -1, 0, 1, 2, 3};
+
+            It should_return_0 = () =>
+                _result.ShouldEqual(0);
+        }
+
+        public class When_the_array_is_an_inverse_sequence
+        {
+            Establish context = () =>
+                A = new [] {3, 2, 1, 0, -1, -2};
+
+            It should_return_the_expected_result = () =>
+                _result.ShouldEqual(4);
+        }
+
+        public class When_the_best_slice_has_3_elements
+        {
+            Establish context = () =>
+                A = new [] {3, 2, -10, 0, -1, -2};
+
+            It should_return_the_expected_result = () =>
+                _result.ShouldEqual(2);
+        }
+
+        public class When_two_slices_produce_the_same_int_average_but_not_the_real
+        {
+            Establish context = () =>
+                A = new [] {1, 0, 0};
+
+            It should_choose_the_minor_real_division_result = () =>
+                _result.ShouldEqual(1);
+        }
+
+        public class When_it_produces_the_most_extreme_case
+        {
+            private Establish context = () =>
+            {
+                A = new int[100000];
+                for (int i = 0; i < 100000; i++)
+                    A[i] = i % 20001 - 10000;
+            };
+
+            It should_return_0 = () =>
+                _result.ShouldEqual(0);
+        }
+
+        static Solution _solution = new Solution();
+        static int[] A;
+        static int _result;
+    }
+}
