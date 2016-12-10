1a4b5859344c7c1f5f011c166fd8be94c4c17b2d
JorgeGamba <jorgegamba@gmail.com>
2016-12-10 13:10:13 +0000

    Lesson 05 - GenomicRangeQuery

---
+++ Lesson05.GenomicRangeQuery/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace Lesson05.GenomicRangeQuery
+{
+    public class Solution
+    {
+        public int[] solution(string S, int[] P, int[] Q)
+        {
+            int prefixSumsSize = S.Length + 1;
+            int[] aCounter = new int[prefixSumsSize];
+            int[] cCounter = new int[prefixSumsSize];
+            int[] gCounter = new int[prefixSumsSize];
+            int[] tCounter = new int[prefixSumsSize];
+            for (int i = 1; i < prefixSumsSize; i++)
+            {
+                aCounter[i] = S[i - 1] == 'A' ? aCounter[i - 1] + 1 : aCounter[i - 1];
+                cCounter[i] = S[i - 1] == 'C' ? cCounter[i - 1] + 1 : cCounter[i - 1];
+                gCounter[i] = S[i - 1] == 'G' ? gCounter[i - 1] + 1 : gCounter[i - 1];
+                tCounter[i] = S[i - 1] == 'T' ? tCounter[i - 1] + 1 : tCounter[i - 1];
+            }
+            int M = P.Length;
+            int[] result = new int[M];
+            for (int i = 0; i < M; i++)
+            {
+                if (aCounter[Q[i] + 1] - aCounter[P[i]] > 0)
+                    result[i] = 1;
+                else if (cCounter[Q[i] + 1] - cCounter[P[i]] > 0)
+                    result[i] = 2;
+                else if (gCounter[Q[i] + 1] - gCounter[P[i]] > 0)
+                    result[i] = 3;
+                else if (tCounter[Q[i] + 1] - tCounter[P[i]] > 0)
+                    result[i] = 4;
+            }
+            return result;
+        }
+    }
+}
+ 
---
+++ Lesson05.GenomicRangeQuery/SolutionSpecs.cs
@@ -0,0 +1,29 @@
+﻿using Machine.Specifications;
+
+namespace Lesson05.GenomicRangeQuery
+{
+    [Subject(typeof(Solution))]
+    public class SolutionSpecs
+    {
+        Because of = () =>
+            _result = _solution.solution(S, P, Q);
+
+        public class on_provided_example
+        {
+            Establish context = () =>
+            {
+                S = "CAGCCTA";
+                P = new [] {2, 5, 0};
+                Q = new [] {4, 5, 6};
+            };
+
+            It should_return_the_expected_result = () =>
+                _result.ShouldEqual(new []{2, 4, 1});
+        }
+
+        static Solution _solution = new Solution();
+        static string S;
+        static int[] P, Q;
+        static int[] _result;
+    }
+}
